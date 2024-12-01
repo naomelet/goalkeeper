f59cdb3c4db531737083ac91242695d3f0c6fc4d
Nao Watanabe <nao-watanabe@mail.nissan.co.jp>
2024-12-01 12:10:31 +0900

    Add initial project files

---
+++ Goalkeeper/Item.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2024  Nissan Motor Co., Ltd. All rights reserved.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GoalkeeperTests/GoalkeeperTests.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2024  Nissan Motor Co., Ltd. All rights reserved.
+//
+
+import Testing
+@testable import Goalkeeper
+
+struct GoalkeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
